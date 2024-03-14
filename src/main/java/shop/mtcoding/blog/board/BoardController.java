9acc5f5a6022a4fac2d212df15f3e9bba54d7136
khh <khh8500@naver.com>
2024-03-14 15:54:02 +0900

    게시글 쓰기 - 연관관계 persist

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,12 +1,14 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
@@ -15,6 +17,15 @@ import java.util.List;
 public class BoardController {
 
     private final BoardRepository boardRepository;
+    private final HttpSession session;
+
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO reqDTO){
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        boardRepository.save(reqDTO.toEntity(sessionUser));
+
+        return "redirect:/";
+    }
 
     @PostMapping("/board/{id}/update")
     public String update(@PathVariable Integer id){
@@ -28,11 +39,6 @@ public class BoardController {
 
     @PostMapping("/board/{id}/delete")
     public String delete(@PathVariable Integer id){
-        return "redirect:/";
-    }
-
-    @PostMapping("/board/save")
-    public String save(){
         return "redirect:/";
     }
 
