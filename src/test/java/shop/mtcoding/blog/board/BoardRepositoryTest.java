766c04b334fe656a5219bbc0a1c80bdaf9eee5c9
khh <khh8500@naver.com>
2024-03-14 12:32:07 +0900

    게시글 목록보기

--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 @Import(BoardRepository.class)
 @DataJpaTest
 public class BoardRepositoryTest {
@@ -13,14 +15,32 @@ public class BoardRepositoryTest {
     private BoardRepository boardRepository;
 
     @Test
-    public void findByIdJoinUser_test(){
+    public void findAll_lazyloading_test() {
+        List<Board> boardList = boardRepository.findAll();
+        boardList.forEach(board -> {
+            System.out.println(board.getUser().getUsername()); // lazy loading
+        });
+    }
+
+    @Test
+    public void findAll_test() {
+        // given
+
+        // when
+        List<Board> boardList = boardRepository.findAll();
+
+        // then
+    }
+
+    @Test
+    public void findByIdJoinUser_test() {
         int id = 1;
 
         boardRepository.findByIdJoinUser(id);
     }
 
     @Test
-    public void findById_test(){
+    public void findById_test() {
         int id = 1;
         System.out.println("start - 1");
         Board board = boardRepository.findById(id);
