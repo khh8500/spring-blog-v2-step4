6e9ec59cc6ad0fb0e25cdd5efb7efbbac857bc0f
khh <khh8500@naver.com>
2024-03-18 12:39:07 +0900

    회원가입 서비스

--- src/main/java/shop/mtcoding/blog/user/UserJPARepository.java
+++ src/main/java/shop/mtcoding/blog/user/UserJPARepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 // 자동 컴퍼넌트 스캔이 된다.
 public interface UserJPARepository extends JpaRepository<User, Integer> {
 
     //@Query("select u from User u where u.username = :username and u.password = :password")
-    User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+    Optional<User> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+    Optional<User> findByUsername(@Param("username") String username);
 }
---
+++ src/main/java/shop/mtcoding/blog/user/UserService.java
@@ -0,0 +1,28 @@
+package shop.mtcoding.blog.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import shop.mtcoding.blog._core.errors.exception.Exception400;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service // IoC 등록
+public class UserService {
+
+    private final UserJPARepository userJPARepository;
+
+    public void 회원가입(UserRequest.JoinDTO reqDTO){ // ssar
+        // 1. 유효성 검사 (컨트롤러 책임)
+
+        // 2. 유저네임 중복검사 (서비스 체크) - DB연결이 필요함
+        Optional<User> userOP = userJPARepository.findByUsername(reqDTO.getUsername());
+
+        if(userOP.isPresent()){
+            throw new Exception400("중복된 유저네임입니다"); // 예외처리
+        }
+
+        userJPARepository.save(reqDTO.toEntity());
+
+    }
+}
