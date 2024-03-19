2e5861bd2b855693788662fe8d6cb7f24f497857
khh <khh8500@naver.com>
2024-03-19 17:13:22 +0900

    댓글 삭제

--- src/main/java/shop/mtcoding/blog/reply/ReplyService.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyService.java
@@ -3,6 +3,7 @@ package shop.mtcoding.blog.reply;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
 import shop.mtcoding.blog._core.errors.exception.Exception404;
 import shop.mtcoding.blog.board.Board;
 import shop.mtcoding.blog.board.BoardJPARepository;
@@ -13,6 +14,17 @@ import shop.mtcoding.blog.user.User;
 public class ReplyService {
     private final BoardJPARepository boardJPARepository;
     private final ReplyJPARepository replyJPARepository;
+
+    @Transactional
+    public void 댓글삭제(int replyId, int sessionUserId){
+        Reply reply = replyJPARepository.findById(replyId)
+                .orElseThrow(() -> new Exception404("없는 댓글을 삭제할 수 없어요"));
+
+        if(reply.getUser().getId() != sessionUserId){
+            throw new Exception403("댓글을 삭제할 권한이 없어요");
+        }
+        replyJPARepository.deleteById(replyId);
+    }
 
     @Transactional
     public void 댓글쓰기(ReplyRequest.SaveDTO reqDTO, User sessionUser){
